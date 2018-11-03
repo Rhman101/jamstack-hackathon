8fe58482016a1b6244c30f95ceb9f3b13a8add8c
mawarijo <mdopure@gmail.com>
2018-11-03 09:12:06 -0700

    switch to React boilerplate

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,28 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'teal',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
+export const Header = ({ startLogout }) => (
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <Link className="header__title" to='/dashboard'>
+          <h1>Boilerplate</h1>
         </Link>
-      </h1>
+        <button
+          className="button button--link"
+          onClick={startLogout}
+        >
+          Log out
+        </button>
+      </div>
     </div>
-  </div>
-)
+  </header>
+);
 
-export default Header
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
