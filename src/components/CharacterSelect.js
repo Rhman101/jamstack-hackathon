f8c5449cf1fed22f40720fe2eedd4b39cb362258
mawarijo <mdopure@gmail.com>
2018-11-04 06:52:23 -0800

    move constants into separate files

--- src/components/CharacterSelect.js
+++ src/components/CharacterSelect.js
@@ -2,22 +2,8 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 import * as routes from "../constants/routes";
+import CHARACTERS from "../constants/characters";
 import OptionCard from "./OptionCard";
-
-const CHARACTERS = [
-  {
-    description:
-      "Wielding nothing but a mighty mind, Character 1 slashes through monsters with razor sharp calculations.",
-    image: "avatar1.jpg",
-    name: "Character 1"
-  },
-  {
-    description:
-      "Pencil and paper swish and flash whenever Character 2 decides to cast some math magic.",
-    image: "avatar2.jpg",
-    name: "Character 2"
-  }
-];
 
 class CharacterSelect extends Component {
   onExit = () => {
